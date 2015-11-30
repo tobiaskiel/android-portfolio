b9f81804afaad6b9dbbc701732248b1b48006f8c
tobiaskiel <tobias.kiel@ericsson.com>
2015-11-30 15:27:39 +0100

    Added click events
    App is now completed for step one

--- MyAppPortfolio/app/src/main/java/com/somedomain/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/somedomain/myappportfolio/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +16,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setupActions();
-
     }
 
-    private void setupActions() {
-        
+    public void BtnClickSpotifyStreamer(View v){
+        ShowToast("This button will launch my Spotify Streamer app!",v);
+    }
+    public void BtnClickLibraryApp(View v) {
+        ShowToast("This button will launch my Library app!",v);
+    }
+    public void BtnClickScoresApp(View v) {
+        ShowToast("This button will launch my Scores app!",v);
     }
 
+    public void BtnClickBuildItBiggerApp(View v) {
+        ShowToast("This button will launch my Build It Bigger app!",v);
+    }
+
+    public void BtnClickxyzReaderApp(View v) {
+        ShowToast("This button will launch my XYZ Reader app!",v);
+    }
+
+    public void BtnClickmyOwnApp(View v) {
+        ShowToast("This button will launch my capstone app!",v);
+    }
+    private void ShowToast(String message, View v)
+    {
+        Toast.makeText(v.getContext(),message,Toast.LENGTH_SHORT).show();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -43,4 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
